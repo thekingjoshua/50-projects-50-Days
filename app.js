0bfed20be224ee491a0de538e6d5fb1719464f90
KingJoshua <80286495+thekingjoshua@users.noreply.github.com>
2023-02-08 20:23:52 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
 'use strict';
 // selecting the required elements
 
-const boxes = document.querySelectorAll('.box');
+const leftContainer = document.querySelector('.left'),
+	rightContainer = document.querySelector('.right'),
+	mainContainer = document.querySelector('.container');
 
-window.addEventListener('scroll', init);
-
-function init() {
-	boxes.forEach((box, i) => {
-		let trigger = (window.innerHeight / 5) * 4;
-		let boxTop = box.getBoundingClientRect().top;
-		console.log(`Trigger:${trigger}`, `Boxtop:${boxTop}`, `number: ${i}`);
-		if (trigger < boxTop) {
-			box.classList.add('show');
-		} else {
-			box.classList.remove('show');
-		}
-	});
-}
+leftContainer.addEventListener('mouseenter', () => {
+	mainContainer.classList.add('hover__left');
+	mainContainer.classList.remove('hover__right');
+});
+leftContainer.addEventListener('mouseenter', () => mainContainer.classList.add('hover__left'));
+rightContainer.addEventListener('mouseenter', () => {
+	mainContainer.classList.add('hover__right');
+	mainContainer.classList.remove('hover__left');
+});
