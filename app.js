77b49ec7b6d5d40352cfd6c90c82db21b2ea9324
KingJoshua <80286495+thekingjoshua@users.noreply.github.com>
2023-02-12 06:04:12 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+'use strict';
+const jokeEl = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokeBtn');
+
+jokeBtn.addEventListener('click', generateJoke);
+
+generateJoke();
+async function generateJoke() {
+	const config = {
+		headers: {
+			Accept: 'application/json',
+		},
+	};
+	const res = await fetch('https://icanhazdadjoke.com', config);
+	const data = await res.json();
+    console.log(data);
+    jokeEl.innerHTML = data.joke
+}
