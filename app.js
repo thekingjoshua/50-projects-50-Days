96d396f7e29dc1350ce70986b06a384e76920dce
KingJoshua <80286495+thekingjoshua@users.noreply.github.com>
2023-02-16 22:50:33 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+'use strict';
+const counters = document.querySelectorAll('.counter');
+
+counters.forEach(counter => {
+	counter.innerHTML = '0';
+
+	const updateCounter = () => {
+		const target = +counter.getAttribute('data-target');
+		const c = +counter.innerHTML;
+		const increment = target / 300;
+
+		if (c < target) {
+			counter.innerText = `${Math.ceil(c + increment)}`;
+			setTimeout(updateCounter, 1);
+		} else {
+			counter.innerText = target;
+		}
+	};
+	updateCounter();
+});
