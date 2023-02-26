257c9193da04bef6e077790831d50bdd609af72a
KingJoshua <80286495+thekingjoshua@users.noreply.github.com>
2023-02-26 11:21:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ setTimeout(getData, 2500)
 
 function getData() {
   header.innerHTML =
-    '<img src="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80" alt="" />'
+    '<img src="https://source.unsplash.com/random/150x150" alt="" />'
   title.innerHTML = 'Lorem ipsum dolor sit amet'
   excerpt.innerHTML =
     'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore perferendis'
